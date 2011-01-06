2ba36537ceeb45459a05307d56ebeedd36669c33
Josh Kearney <josh@jk0.org>
2011-01-06 12:44:55 -0600

    Give the option to disable PEP8 or test checks

--- pre_commit_hook.py
+++ pre_commit_hook.py
@@ -8,6 +8,10 @@ from bzrlib import branch
 from bzrlib import errors
 
 
+CHECK_PEP8 = True
+CHECK_TESTS = False
+
+
 def pre_commit_hook(
     local,
     master,
@@ -17,12 +21,20 @@ def pre_commit_hook(
     future_revid,
     tree_delta,
     future_tree):
-    """Ensure code is PEP8 compliant and all unit tests pass"""
-    if subprocess.call(["pep8", "-r", "./nova"]):
-        raise errors.BzrError("Code is not PEP8 compliant.")
+    """Ensure code is PEP8 compliant and all unit tests pass."""
+    if CHECK_PEP8:
+        if subprocess.call([
+            "pep8",
+            "--repeat",
+            "--show-source",
+            "--show-pep8",
+            "./nova"]):
+            raise errors.BzrError("Code is not PEP8 compliant.")
 
-    if subprocess.call("./run_tests.sh"):
-        raise errors.BzrError("Unit tests did not pass.")
+    if CHECK_TESTS:
+        if subprocess.call("./run_tests.sh"):
+            raise errors.BzrError("Unit tests did not pass.")
+
 
 branch.Branch.hooks.install_named_hook(
     "pre_commit",
